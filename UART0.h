17dcff72c96fb1949b7f64406d7e8cfa7d698df7
hector toral <hectortoraltrabajos@gmail.com>
2022-12-21 23:11:03 +0100

    mh

--- UART0.h
+++ UART0.h
@@ -16,10 +16,22 @@
 
 enum { BUFFER_ENVIO_SIZE = 32 };
 
+/**
+ * @brief Configura el envío de caracteres a través del periférico UART0.
+ */
 void uart0_iniciar(void);
 
+/**
+ * @brief Inicia el envío de una cadena de caracteres tipo C, enviando el
+ *        primer caracter.
+ * @param array Cadena a enviar.
+ */
 void uart0_enviar_array(const char* array);
 
+/**
+ * @brief Envía el siguiente caracter de la cadena.
+ * @return true si quedaban caracteres por enviar.
+ */
 int uart0_continuar_envio(void);
 
 #endif
